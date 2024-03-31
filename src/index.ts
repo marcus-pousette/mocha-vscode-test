d97cb5c312f642147f5183a0d869f1538ea92670
Marcus Pousette <marcus.pousette@quantleaf.com>
2024-03-31 21:09:18 +0200

    ic

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export const subtract = (a: number, b: number) => a - b
+
+
+
---
+++ test/index.spec.ts
@@ -0,0 +1,18 @@
+import { expect } from 'chai'
+import { subtract } from '../src/index.js'
+
+ 
+
+
+
+
+
+
+
+
+
+describe("hello", () => {
+   it('test', () => {
+     expect(subtract(1, 2)).to.equal(-1) 
+   })
+})
